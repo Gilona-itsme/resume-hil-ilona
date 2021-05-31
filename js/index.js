0b3954d7babbb2c561e7432aa1e0a23ee9825434
Ilona <ilona.gil@outlook.com>
2021-05-31 11:45:25 +0300

    update adaptive

--- js/index.js
+++ js/index.js
@@ -5,3 +5,14 @@ window.onload = function () {
 		document.body.classList.remove("loaded_hiding");
 	}, 500);
 };
+
+// const column = document.querySelector(".resume");
+
+// function changeClass() {
+// 	column.classList.remove("column__left").add("column__right");
+// }
+
+// // Crude way to check that the screen is at least 1024x768
+// if (window.matchMedia("(min-width: 768px)").matches) {
+// 	changeClass();
+// }
